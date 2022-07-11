d707304c2b08267380847bd58b153f945a30fcb1
Isadora Gadelha Lima <isadora.gadelha@outlook.com>
2022-07-11 09:26:03 -0300

    Adicionando schemas do cubeJs das adquirentes

---
+++ FaixaParcelamento.js
@@ -0,0 +1,48 @@
+cube(`FaixaParcelamento`, {
+  sql: `SELECT * FROM dimensao.faixa_parcelamento`,
+  
+  preAggregations: {
+    // Pre-Aggregations definitions go here
+    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started  
+  },
+  
+  joins: {
+    
+  },
+  
+  measures: {
+    count: {
+      type: `count`,
+      drillMembers: [dateFrom, dateTo]
+    }
+  },
+  
+  dimensions: {
+    skFaixaParcelamento: {
+      sql: `sk_faixa_parcelamento`,
+      type: `number`,
+      primaryKey: true
+    },
+    faixaParcelamento: {
+      sql: `descricao`,
+      type: `string`
+    },
+    
+    codigoParcelamento: {
+      sql: `codigo`,
+      type: `string`
+    },
+    
+    dateFrom: {
+      sql: `date_from`,
+      type: `time`
+    },
+    
+    dateTo: {
+      sql: `date_to`,
+      type: `time`
+    }
+  },
+  
+  dataSource: `default`
+});
